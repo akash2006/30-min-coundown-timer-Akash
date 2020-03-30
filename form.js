b98d188017ee3024686b28841d8d8794bfb9f3c7
Akash Maity <59837849+akash2006@users.noreply.github.com>
2020-03-30 17:11:30 +0530

    Update form.js

--- form.js
+++ form.js
@@ -1,35 +1,43 @@
 class Form{
-    constructor(){
+    constructor(){       
         this.input = createInput("Enter your name");
+        this.button = createButton("Submit");                    
         // this.input.html(setAttribute("placeholder","Enter your name"));    
-        this.greeting = createElement('h2');
-        this.button = createButton("Submit");
+        this.greeting = createElement('h2');        
         // this.button.parent("#submit");
         this.instruct = createElement('h3');
         this.result = createElement('h2');
         this.time = createElement('h3');
         this.breaks = createElement('h3');
     }
+    // hide(){
+    //     if(timerState === "off"){
+    //         this.input.hide();
+    //         this.button.hide();
+    //     }
+    // }
     display(){
-        this.input.position(10,130);
-        this.button.position(200,130)
-        if(timerState === "off"){
-            this.button.mousePressed(()=>{
-                timer.updateState("submitted");
-                // getElementByName(this.input);
-                console.log(this.input.value());
-                this.input.hide();
-                this.button.hide();
-                user.name = this.input.value();
-                user.getUserCount();
-                userCount+=1;
-                user.index = userCount;
-                user.updateCount(userCount);
-                
-                this.greeting.html("Hello "+ user.name);
-                this.instruct.html("Please click on 'start' button to start counting");
-            })  
-        }
+        // if(timerState === "on"){
+            this.input.position(10,130);
+            this.button.position(200,130)
+            if(timerState === "on"){
+                this.button.mousePressed(()=>{
+                    timer.updateState("submitted");
+                    // getElementByName(this.input);
+                    console.log(this.input.value());
+                    this.input.hide();
+                    this.button.hide();
+                    user.name = this.input.value();
+                    user.getUserCount();
+                    userCount+=1;
+                    user.index = userCount;
+                    user.updateCount(userCount);
+                    
+                    this.greeting.html("Hello "+ user.name);
+                    this.instruct.html("Please click on 'start' button to start counting");
+                })  
+            }
+        // }    
     }
     // getTime(){
     //     finSeconds = seconds;
